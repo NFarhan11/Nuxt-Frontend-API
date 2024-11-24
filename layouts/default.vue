bf4023680ed842c06686f1a3d22fbaf91e2c821c
NFarhan11 <najmifarhan9903@gmail.com>
2024-11-24 14:35:10 +0800

    added GET request fetch

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li><NuxtLink to="/">Index</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <!-- <li><NuxtLink to="/posts/{ $route }">Nothing</NuxtLink></li> -->
+            </ul>
+        </nav>
+    </header>
+    <div>
+        <p class="default-layout">Default layout</p>
+        <slot></slot>
+    </div>
+</template>
+
+<style>
+.default-layout {
+    color:blueviolet
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <Head>
+        <Title>Index page</Title>
+    </Head>
+    <div>
+        <h1 class="m-2 border border-black">Welcome to the homepage</h1>
+        <AppAlert>
+            This is an auto-imported component
+        </AppAlert>
+    </div>
+
+    <div v-if="data == null">
+        No data
+    </div>
+    <div v-else>
+        <ul v-for="post in data">
+            <li>{{ post.title }}, {{ post.body }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { data } = await useFetch('http://localhost:8000/api/posts/')
+</script>
